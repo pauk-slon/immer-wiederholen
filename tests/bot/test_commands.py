daa321b186bc905ae887a3434aa73998b721fa28
Dmitry Kolyagin <dmitry.kolyagin@gmail.com>
2026-07-05 14:06:49 +0200

    Add tests for `/start` command (#36)

--- tests/bot/test_commands.py
+++ tests/bot/test_commands.py
@@ -1,5 +1,7 @@
 import dataclasses
 
+import pytest
+
 from aiogram.fsm.context import FSMContext
 from aiogram.types import InlineKeyboardMarkup
 
@@ -9,6 +11,29 @@ from wiederholen.exercises import School
 
 from tests.plugins.aiogram import FeedRawUpdate
 from tests.plugins.exercises import make_exercise
+
+
+class TestStartCommand:
+    async def test_defaults_to_ru(
+        self,
+        feed_raw_update: FeedRawUpdate,
+    ) -> None:
+        send_message = await feed_raw_update("/start")
+
+        assert send_message.text == RU.start
+
+    @pytest.mark.parametrize("language,expected", [("ru", RU.start), ("en", EN.start)])
+    async def test_responds_in_current_language(
+        self,
+        state: FSMContext,
+        feed_raw_update: FeedRawUpdate,
+        language: str,
+        expected: str,
+    ) -> None:
+        await state.update_data(language=language)
+        send_message = await feed_raw_update("/start")
+
+        assert send_message.text == expected
 
 
 class TestWiederholenCommand:
