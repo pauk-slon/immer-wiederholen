52e9176e1d614a09f0c94da9d85a9e25ebb81a94
Dmitry Kolyagin <dmitry.kolyagin@gmail.com>
2026-07-12 10:05:59 +0200

    Extract `callback_query_factory` (#49)

--- tests/plugins/aiogram.py
+++ tests/plugins/aiogram.py
@@ -13,6 +13,7 @@ from wiederholen.bot import dispatcher as _dp
 
 
 type MessageFactory = Callable[..., dict]
+type CallbackQueryFactory = Callable[..., dict]
 type FeedRawUpdate = Callable[..., Awaitable[list[Any]]]
 type FeedCallbackQuery = Callable[..., Awaitable[list[Any]]]
 type FeedMessage = Callable[..., Awaitable[list[Any]]]
@@ -21,16 +22,6 @@ type FeedMessage = Callable[..., Awaitable[list[Any]]]
 @pytest.fixture
 def bot_token() -> str:
     return "1234567890:AAHHte3GRDo4KzHsY6U6xZTMSfI7xv3c_xY"
-
-
-@pytest.fixture
-def bot(bot_token: str) -> Bot:
-    return Bot(token=bot_token)
-
-
-@pytest.fixture
-def dispatcher() -> Dispatcher:
-    return _dp
 
 
 @pytest.fixture
@@ -67,24 +58,9 @@ def message_factory(user_id: int, chat_id: int) -> MessageFactory:
 
 
 @pytest.fixture
-def feed_raw_update(bot: Bot, dispatcher: Dispatcher) -> FeedRawUpdate:
-    async def factory(raw_update: dict, **kwargs) -> list[Any]:
-        mock_request = AsyncMock(return_value=True)
-        with patch.object(bot.session, "make_request", mock_request):
-            await dispatcher.feed_raw_update(bot, raw_update, **kwargs)
-        return [call.args[1] for call in mock_request.call_args_list]
-
-    return factory
-
-
-@pytest.fixture
-def feed_callback_query(
-    feed_raw_update: FeedRawUpdate,
-    user_id: int,
-    chat_id: int,
-) -> FeedCallbackQuery:
-    async def factory(data: str | None, **kwargs) -> list[Any]:
-        raw_update = {
+def callback_query_factory(user_id: int, chat_id: int) -> CallbackQueryFactory:
+    def factory(data: str | None) -> dict:
+        return {
             "update_id": 2,
             "callback_query": {
                 "id": "test_callback_id",
@@ -100,6 +76,38 @@ def feed_callback_query(
                 "data": data,
             },
         }
+
+    return factory
+
+
+@pytest.fixture
+def bot(bot_token: str) -> Bot:
+    return Bot(token=bot_token)
+
+
+@pytest.fixture
+def dispatcher() -> Dispatcher:
+    return _dp
+
+
+@pytest.fixture
+def feed_raw_update(bot: Bot, dispatcher: Dispatcher) -> FeedRawUpdate:
+    async def factory(raw_update: dict, **kwargs) -> list[Any]:
+        mock_request = AsyncMock(return_value=True)
+        with patch.object(bot.session, "make_request", mock_request):
+            await dispatcher.feed_raw_update(bot, raw_update, **kwargs)
+        return [call.args[1] for call in mock_request.call_args_list]
+
+    return factory
+
+
+@pytest.fixture
+def feed_callback_query(
+    feed_raw_update: FeedRawUpdate,
+    callback_query_factory: CallbackQueryFactory,
+) -> FeedCallbackQuery:
+    async def factory(data: str | None, **kwargs) -> list[Any]:
+        raw_update = callback_query_factory(data)
         return await feed_raw_update(raw_update, **kwargs)
 
     return factory
